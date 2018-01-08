cfb441ba7eac14357c6c75f5d50b6b99474f3de3
kouhei <kouhei@HOME-PC>
2018-01-08 10:19:04 +0900

    d3js add

---
+++ DBRefactoringWEB/Default2.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default2.aspx.cs" Inherits="Default2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<link rel="stylesheet" href="~/Content/style.css"/>
+    <title>DBリファクタリング</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br>
+            <asp:Button ID="Button1" runat="server" Text="クリックしてください" OnClick="Button1_Click" />
+            <asp:GridView ID="columnlist1" runat="server"></asp:GridView>
+        </div>
+        <div class="svg_area" id="svg_area"></div>
+        <!--<script type="text/javascript" src="libs/d3.js" charset="utf-8"></script>-->
+        <script type="text/javascript" src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+        <!-- scripts -->
+        <script type="text/javascript" src="http://localhost:55129/Scripts/main.js" charset="utf-8"></script>
+    </form>
+</body>
+</html>
---
+++ DBRefactoringWEB/Default2.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    private string tablename;
+    private int colCnt;
+    private List<string> colNsmeList;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        tablename = "社員";
+        SqlConnection con
+            = new SqlConnection();
+        con.ConnectionString = "Data Source=HOME-PC\\SQLEXPRESS;Initial Catalog=testDB;Integrated Security=True"; //接続情報を入れる
+        con.Open();
+
+        //クエリーの生成
+        SqlCommand sqlCom = new SqlCommand();
+
+        //クエリー送信先及びトランザクションの指定
+        sqlCom.Connection = con;
+        //sqlCom.Transaction = this.sqlTran;
+
+        //クエリー文の指定
+        sqlCom.CommandText = "SELECT * FROM " + tablename + ";";
+
+        //データテーブルを作成するためのアダプタ
+        SqlDataAdapter sqlAda = new SqlDataAdapter();
+        sqlAda.SelectCommand = sqlCom;
+
+        DataTable dsgrid = new DataTable();
+        dsgrid.Columns.Add("Column", typeof(string));
+        dsgrid.Columns.Add("word2vec距離", typeof(string));
+        //dsにテーブルデータを代入
+        DataTable ds = new DataTable();
+        sqlAda.Fill(ds);
+        colCnt = ds.Columns.Count;
+        colNsmeList = new List<string>();
+        //List<String> columnList = new List<string>();
+        foreach (DataColumn clm in ds.Columns)
+        {
+            //columnList.Add(clm.ColumnName);
+            //var sim =getSimilarity(tablename, clm.ColumnName);
+            var sim = "0";
+            string[] row = new string[] { clm.ColumnName, sim };
+            dsgrid.Rows.Add(row);
+            colNsmeList.Add(clm.ColumnName);
+        }
+        //dsgrid.Rows[10]["word2vec距離"] = "0.1";
+
+        columnlist1.DataSource = dsgrid;
+        columnlist1.DataBind();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Label1.Text = "Hello";
+    }
+}
